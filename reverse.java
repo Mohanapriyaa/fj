c8208b3aa6a7afbfce7ecde0129299e2acee8d37
Mohanapriyaa <priyaannakodi1996@gmail.com>
2016-08-07 11:26:46 +0530

    Add files via upload

--- reverse.java
+++ reverse.java
@@ -3,7 +3,7 @@ public class reverse {
 
 	
 	public static void main(String[] args) {
-    String s2="butterfly";
+    String s2="3124512";
     StringBuffer s3=new StringBuffer(s2);
     System.out.println(s3.reverse());
 	}
