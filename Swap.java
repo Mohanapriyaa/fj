6ae58a920ebe5c96f31eddb2754b514bc75bcf55
Mohanapriyaa <priyaannakodi1996@gmail.com>
2016-08-07 11:54:08 +0530

    Add files via upload

---
+++ Swap.java
@@ -0,0 +1,17 @@
+
+public class Swap {
+
+	
+	public static void main(String[] args) {
+     int a=8;
+     int b=3;
+     System.out.println(a);
+     System.out.println(b);
+     int temp=a;
+     a=b;
+     b=temp;
+     System.out.println(a);
+     System.out.println(b);
+	}
+
+}
